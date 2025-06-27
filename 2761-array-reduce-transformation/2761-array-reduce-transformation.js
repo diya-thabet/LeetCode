27b3df61fa579f0f05704098b5406b8d69ba9b1b
Dhia Eddine Thabet <93912178+diya-thabet@users.noreply.github.com>
2025-06-27 21:58:23 +0100

    Time: 46 ms (42.64%), Space: 51.6 MB (99.95%) - LeetHub

--- 2761-array-reduce-transformation/2761-array-reduce-transformation.js
+++ 2761-array-reduce-transformation/2761-array-reduce-transformation.js
@@ -5,11 +5,10 @@
  * @return {number}
  */
 var reduce = function(nums, fn, init) {
-    
-    let res = init
-    for (let i = 0; i < nums.length; i++) {
-        res = fn(res, nums[i])
-        
+    let res = init;
+    const len = nums.length;
+    for (let i = 0; i < len; i++) {
+        res = fn(res, nums[i]);
     }
-    return res
+    return res;
 };
