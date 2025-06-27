27670aa301f39f0879eaacc2301986306181e299
Dhia Eddine Thabet <93912178+diya-thabet@users.noreply.github.com>
2025-06-27 22:13:13 +0100

    Time: 40 ms (67.6%), Space: 52.2 MB (97.84%) - LeetHub

---
+++ 2746-filter-elements-from-array/2746-filter-elements-from-array.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number[]} arr
+ * @param {Function} fn
+ * @return {number[]}
+ */
+var filter = function(arr, fn) {
+    let res =[]; 
+    let n =arr.length; 
+    for(let i = 0 ; i<n ; i++){
+        if(fn(arr[i], i)){
+            res.push(arr[i]); 
+        }
+    }
+
+    return res; 
+};
