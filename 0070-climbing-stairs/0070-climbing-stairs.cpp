ee17edcc5b56126d1ad64cfd9dd1de05ed84b90f
Dhia Eddine Thabet <93912178+diya-thabet@users.noreply.github.com>
2025-06-28 21:40:00 +0100

    Time: 0 ms (100%), Space: 8 MB (58.48%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -4,11 +4,7 @@ public:
         
         if(n<=2) return n; 
         n++;
-        vector<int> v(n); 
-        v[n-1]=1; v[n-2]=1; 
-        for(int i = n-3; i>=0; i--){
-            v[i]=v[i+1]+v[i+2]; 
-        }
-        return v[0]; 
+        double phi = (1 + sqrt(5)) / 2;
+        return round(pow(phi, n) / sqrt(5));
     }
 };
