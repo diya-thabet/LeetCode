47be590a956ddeb11d4222c2aae86d02ceb4d007
Dhia Eddine Thabet <93912178+diya-thabet@users.noreply.github.com>
2025-07-01 20:29:10 +0100

    Time: 0 ms (100%), Space: 9 MB (49.4%) - LeetHub

---
+++ 3567-convert-date-to-binary/3567-convert-date-to-binary.cpp
@@ -0,0 +1,49 @@
+class Solution {
+public:
+    string convertDateToBinary(string date) {
+        string res = "", sYear = "", sMonth = "", sDay = ""; 
+        int t  = 1; 
+        
+        for(char c : date){
+            switch (t){
+                case 1:
+                    if(c=='-'){
+                        t++;
+                        continue; 
+                    }
+                    sYear+=c; 
+                    break; 
+                case 2:
+                    if(c=='-'){
+                        t++;
+                        continue; 
+                    }
+                    sMonth+=c; 
+                    break;
+                case 3:
+                    if(c=='-'){
+                        t++;
+                        continue; 
+                    }
+                    sDay+=c; 
+                    break; 
+            }
+        }
+        int year = stoi(sYear), month=stoi(sMonth), day=stoi(sDay);
+        sYear = intToBinaryString(year); 
+        sMonth = intToBinaryString(month);
+        sDay = intToBinaryString(day); 
+
+        return sYear + '-' + sMonth + '-' + sDay; 
+    }
+
+    string intToBinaryString(int n) {
+        if (n == 0) return "0";
+        std::string result;
+        while (n > 0) {
+            result = char('0' + (n % 2)) + result;
+            n /= 2;
+        }
+        return result;
+    }
+};
