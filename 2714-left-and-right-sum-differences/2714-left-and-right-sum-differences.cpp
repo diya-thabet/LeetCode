3c7a0512381e5c9d91c93023ef69674d4611ec41
Dhia Eddine Thabet <93912178+diya-thabet@users.noreply.github.com>
2025-07-03 15:16:33 +0100

    Time: 0 ms (100%), Space: 15.7 MB (6.59%) - LeetHub

--- 2714-left-and-right-sum-differences/2714-left-and-right-sum-differences.cpp
+++ 2714-left-and-right-sum-differences/2714-left-and-right-sum-differences.cpp
@@ -1,18 +1,25 @@
 class Solution {
 public:
     vector<int> leftRightDifference(vector<int>& nums) {
-        int left = 0, right = 0;
-        vector<int> res; 
-        for(int i = 0 ; i<nums.size(); i++ ){
-            for(int j = i+1 ; j<nums.size(); j++ ){
-                left+=nums[j];
-            }
-            if(i>0) right += nums[i-1];
-            res.push_back(abs(left-right));
-            cout << left << " " << right << endl ; 
-            left = 0;
+        int n = nums.size();
+        vector<int> res;
+        int left[1000], right[1000];
+        memset(left, 0, sizeof(left));
+        memset(right, 0, sizeof(right));
+
+        for (int i = 1; i < n; ++i) {
+            left[i] = left[i - 1] + nums[i - 1];
         }
 
-        return res; 
+        for (int i = n - 2; i >= 0; --i) {
+            right[i] = right[i + 1] + nums[i + 1];
+        }
+
+        for (int i = 0; i < n; ++i) {
+            int x = abs(left[i] - right[i]);
+            res.push_back(x);
+        }
+
+        return res;
     }
 };
