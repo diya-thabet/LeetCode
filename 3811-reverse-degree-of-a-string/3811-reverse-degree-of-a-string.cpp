5280cf0b0fcb256ce5e3f083f82c3ecf0b903ed0
Dhia Eddine Thabet <93912178+diya-thabet@users.noreply.github.com>
2025-07-03 16:16:08 +0100

    Time: 0 ms (100%), Space: 9.8 MB (49.06%) - LeetHub

--- 3811-reverse-degree-of-a-string/3811-reverse-degree-of-a-string.cpp
+++ 3811-reverse-degree-of-a-string/3811-reverse-degree-of-a-string.cpp
@@ -3,7 +3,7 @@ public:
     int reverseDegree(string s) {
         int res = 0; 
         for(int i =0; i<s.length() ; i++){
-            res+= ((26-(s[i]-'a')))*(i+1); 
+            res+= (26-(s[i]-'a'))*(i+1); 
         }
 
         return res; 
