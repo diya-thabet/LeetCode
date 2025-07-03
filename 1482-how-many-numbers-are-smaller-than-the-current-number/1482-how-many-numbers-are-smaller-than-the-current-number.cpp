c0b0ed05ed2e40fdc7485076002c218dfc25fa97
Dhia Eddine Thabet <93912178+diya-thabet@users.noreply.github.com>
2025-07-03 16:44:04 +0100

    Time: 18 ms (8.57%), Space: 14.2 MB (60.05%) - LeetHub

--- 1482-how-many-numbers-are-smaller-than-the-current-number/1482-how-many-numbers-are-smaller-than-the-current-number.cpp
+++ 1482-how-many-numbers-are-smaller-than-the-current-number/1482-how-many-numbers-are-smaller-than-the-current-number.cpp
@@ -4,22 +4,18 @@ public:
         int i =0;
         vector<int> res; 
         while(i<nums.size()){
-            int left = 0, right = 0; 
-            
+            int left = 0, right = 0;
             if(i>0){
                 for(int j = i-1; j>=0 ; j--){
                     if(nums[j]<nums[i]) left++; 
                 }
             }
-            
             if(i<nums.size()){
                 for(int j = i+1; j<nums.size() ; j++){
                     if(nums[j]<nums[i]) right++; 
                 }
             }
-            
             res.push_back(left + right); 
-            cout << left << " " << right << endl;
             i++;
         }
 
