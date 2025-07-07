1cc4556fdcf3a489be482bd330f52ab49ed35294
Dhia Eddine Thabet <93912178+diya-thabet@users.noreply.github.com>
2025-07-07 22:32:33 +0100

    Time: 15 ms (0.77%), Space: 7.9 MB (53.57%) - LeetHub

---
+++ 1610-xor-operation-in-an-array/1610-xor-operation-in-an-array.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int xorOperation(int n, int start) {
+        int x = 0; 
+        for(int i = 0; i<n; i++){
+            cout << start+2*i << endl; 
+            x^= start+2*i;
+        }
+        return x;
+    }
+};
