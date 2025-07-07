5fc87d4d6daab4ec38cb19b6d521d4b71bf3af69
Dhia Eddine Thabet <93912178+diya-thabet@users.noreply.github.com>
2025-07-07 23:17:32 +0100

    Time: 0 ms (100%), Space: 8.6 MB (48.38%) - LeetHub

--- 2752-sum-multiples/2752-sum-multiples.cpp
+++ 2752-sum-multiples/2752-sum-multiples.cpp
@@ -1,13 +1,20 @@
 class Solution {
 public:
-    int sumDiv(int n, int k) {
-        int m = n / k;
-        return k * m * (m + 1) / 2;
-    }
-
     int sumOfMultiples(int n) {
-        return sumDiv(n, 3) + sumDiv(n, 5) + sumDiv(n, 7)
-             - sumDiv(n, 15) - sumDiv(n, 21) - sumDiv(n, 35)
-             + sumDiv(n, 105);
+        int sum=0;
+        for(int i=0;i<=n;i++){
+            if(i%3==0){
+            sum+=i;
+            }else if(i%5==0 ){
+             sum+=i;
+            }else if(i%7==0){
+                sum+=i;
+                
+            }else{
+                sum+=0;
+            }
+        }
+        return sum;
+        
     }
 };
