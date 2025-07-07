50b3b2e526caf3965fe9dd47befa9876eee4494f
Dhia Eddine Thabet <93912178+diya-thabet@users.noreply.github.com>
2025-07-07 22:07:55 +0100

    Time: 0 ms (100%), Space: 27 MB (98.65%) - LeetHub

--- 3555-final-array-state-after-k-multiplication-operations-i/3555-final-array-state-after-k-multiplication-operations-i.cpp
+++ 3555-final-array-state-after-k-multiplication-operations-i/3555-final-array-state-after-k-multiplication-operations-i.cpp
@@ -3,8 +3,7 @@ public:
     vector<int> getFinalState(vector<int>& nums, int k, int multiplier) {
         for(int j = 0; j<k ; j++){
 
-            int minn = 0; 
-            for(int i : nums) minn = max(i, minn);
+            int minn = INT_MAX; 
             int index = 0; 
             for(int i=0 ; i<nums.size() ; i++){
                 if(nums[i]<minn) {minn = nums[i] ; index= i; }
