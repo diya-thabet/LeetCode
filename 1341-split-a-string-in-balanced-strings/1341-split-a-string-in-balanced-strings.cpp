dc70ccc4ddee8e6145468b5c9db078720fa4f3c7
Dhia Eddine Thabet <93912178+diya-thabet@users.noreply.github.com>
2025-07-07 22:26:15 +0100

    Time: 0 ms (100%), Space: 8.5 MB (16.57%) - LeetHub

--- 1341-split-a-string-in-balanced-strings/1341-split-a-string-in-balanced-strings.cpp
+++ 1341-split-a-string-in-balanced-strings/1341-split-a-string-in-balanced-strings.cpp
@@ -1,11 +1,11 @@
 class Solution {
 public:
     int balancedStringSplit(string s) {
-        int l = 0, r = 0; int res = 0; 
+        int l = 0; int res = 0; 
         for(char c : s){
             if(c == 'L') l++; 
-            if(c=='R') r++; 
-            if(l==r) res++; 
+            if(c=='R') l--; 
+            if(!l) res++; 
         }
 
         return res; 
