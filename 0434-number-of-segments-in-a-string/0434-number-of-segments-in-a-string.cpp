94f26b5e8d0a60386ee0019fdb51205794fed2c7
Dhia Eddine Thabet <93912178+diya-thabet@users.noreply.github.com>
2025-07-08 16:07:47 +0100

    Time: 0 ms (100%), Space: 8.3 MB (19.14%) - LeetHub

--- 0434-number-of-segments-in-a-string/0434-number-of-segments-in-a-string.cpp
+++ 0434-number-of-segments-in-a-string/0434-number-of-segments-in-a-string.cpp
@@ -1,10 +1,19 @@
 class Solution {
 public:
     int countSegments(string s) {
-        int words = 0;
-        for(int i = 0, n = s.length(); i < n; ++i){
-            if (s[i] != ' ' && s[i + 1] == ' ' || s[i] != ' ' && s[i + 1] == '\0') ++words;
-        } 
-        return words;
+        s = s+' ';
+        int n = s.length();
+        int count=0;
+        int i=0;
+        while(s[i]==' ') i++;
+        for(;i<n;i++){
+            if(s[i]==' '){
+                while(s[i]==' '){
+                    i++;
+                }
+                count++;
+            }
+        }
+        return count;
     }
 };
