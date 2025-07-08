c2147dddbcc11f96b2508c26f78bea89c0c8a199
Dhia Eddine Thabet <93912178+diya-thabet@users.noreply.github.com>
2025-07-08 10:29:02 +0100

    Time: 0 ms (100%), Space: 8.4 MB (14.52%) - LeetHub

---
+++ 0742-to-lower-case/0742-to-lower-case.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    string toLowerCase(string s) {
+        int diff = 'a'-'A'; 
+        for (int i = 0; i<s.size(); i++){
+            if(s[i]<='Z' && s[i] >='A'){
+                s[i]+=diff; 
+            }
+        }
+
+        return s; 
+    }
+};
