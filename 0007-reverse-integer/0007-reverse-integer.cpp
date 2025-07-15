45ab386128b6f72b50b8f49ebc6c3d3af7572f4b
Dhia Eddine Thabet <93912178+diya-thabet@users.noreply.github.com>
2025-07-15 11:50:06 +0100

    Time: 5 ms (19.44%), Space: 8.5 MB (82.41%) - LeetHub

--- 0007-reverse-integer/0007-reverse-integer.cpp
+++ 0007-reverse-integer/0007-reverse-integer.cpp
@@ -1,19 +1,18 @@
 class Solution {
 public:
     int reverse(int x) {
-        if(x<= INT_MIN || x>=INT_MAX) return 0; 
-        
+        int rev = 0;
 
-        // inverse the x 
-        int sign = x>=0 ? 1 : -1; 
-        string s = to_string(abs(x)); 
-        string ss = ""; 
-        for(int i = s.size()-1 ; i>=0 ; i--){
-            ss+= s[i]; 
+        while (x != 0) {
+            int pop = x % 10;
+            x /= 10;
+
+            if (rev > INT_MAX / 10 ) return 0;
+            if (rev < INT_MIN / 10 ) return 0;
+
+            rev = rev * 10 + pop;
         }
 
-        long long res = sign * stoll(ss); 
-
-        return  res <= INT_MIN || res >= INT_MAX ? 0 : res; 
+        return rev;
     }
 };
