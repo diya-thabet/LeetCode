c003ba094cad620ebd19f4487a4afca65547018e
Dhia Eddine Thabet <93912178+diya-thabet@users.noreply.github.com>
2025-07-17 19:59:20 +0100

    Time: 0 ms (100%), Space: 10.5 MB (80.7%) - LeetHub

--- 0338-counting-bits/0338-counting-bits.cpp
+++ 0338-counting-bits/0338-counting-bits.cpp
@@ -1,28 +1,19 @@
-
-
 class Solution {
 public:
     vector<int> countBits(int n) {
-        vector<int> res; 
-        res.push_back(0); 
-        for(int i = 1 ; i<=n ; i++){
-            int c = i, bit = 0; 
-            while(c){
-                if(c&1) bit++;
-                c=c>>1; 
-            }
-            res.push_back(bit);
+        vector<int> v(n + 1);
+        v.at(0) = 0;
+        if (n > 0) {
+            v.at(1) = 1;
         }
-
-        return res; 
+        for (int i = 2; i < n + 1; i++) {
+            if (i % 2 != 0) {
+                v.at(i) = v.at(i - 1) + 1;
+            }
+            else {
+                v.at(i) = v.at((i / 2));
+            }
+        }
+        return v;
     }
 };
-
-#define LC_HACK
-#ifdef LC_HACK
-const auto __ = []() {
-  struct ___ { static void _() { std::ofstream("display_runtime.txt") << 0 << '\n'; } };
-  std::atexit(&___::_);
-  return 0;
-}();
-#endif
