6ac9a97180ce50ddbdb7c7576a3b2cd65e32d7fa
Dhia Eddine Thabet <93912178+diya-thabet@users.noreply.github.com>
2025-07-18 19:40:48 +0100

    Time: 2 ms (53.82%), Space: 10.3 MB (75.23%) - LeetHub

---
+++ 0657-robot-return-to-origin/0657-robot-return-to-origin.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    bool judgeCircle(string moves) {
+        int i = 0, j = 0; 
+        for(char c : moves){
+            if(c=='R') i++; 
+            if(c=='L') i--; 
+            if(c=='U') j++; 
+            if(c=='D') j--; 
+        }
+
+        return i==0 && j==0; 
+    }
+};
