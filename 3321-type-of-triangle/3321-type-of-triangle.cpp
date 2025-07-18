6313e744d686aeb00644458843665f1013ad5fd3
Dhia Eddine Thabet <93912178+diya-thabet@users.noreply.github.com>
2025-07-18 19:27:44 +0100

    Time: 0 ms (100%), Space: 22.9 MB (67.83%) - LeetHub

--- 3321-type-of-triangle/3321-type-of-triangle.cpp
+++ 3321-type-of-triangle/3321-type-of-triangle.cpp
@@ -1,11 +1,12 @@
 class Solution {
 public:
     string triangleType(vector<int>& nums) {
-        set<int>s(nums.begin(),nums.end());
-        sort(nums.begin(),nums.end());
-        if((nums[0] + nums[1])<=nums[2]) return "none";
-        if(s.size() == 1) return "equilateral";
-        if(s.size() == 2) return "isosceles";
-        return "scalene";
+        if(nums[0] + nums[1] > nums[2] && nums[0] + nums[2] > nums[1] && nums[2] + nums[1] > nums[0] ) {
+            if(nums[0]==nums[1] && nums[1]==nums[2]) return "equilateral";
+            if(nums[0]==nums[1] || nums[1]==nums[2] || nums[0]==nums[2]) return "isosceles";
+            else return "scalene";
+        } else return "none"; 
+
+        
     }
 };
