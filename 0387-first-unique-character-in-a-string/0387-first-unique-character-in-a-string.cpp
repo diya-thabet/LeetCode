cf00e339039c40fd1e99c7a31b9ede53f0a8b179
Dhia Eddine Thabet <93912178+diya-thabet@users.noreply.github.com>
2025-07-18 18:55:45 +0100

    Time: 3 ms (84.11%), Space: 14.6 MB (77.62%) - LeetHub

---
+++ 0387-first-unique-character-in-a-string/0387-first-unique-character-in-a-string.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int firstUniqChar(string s) {
+        int alpha[26]; 
+        for(char c : s) alpha[c - 'a']++; 
+        for(int i= 0; i<s.size(); i++){
+            if(alpha[s[i]-'a']==1) return i;
+        }
+
+        return -1; 
+    }
+};
