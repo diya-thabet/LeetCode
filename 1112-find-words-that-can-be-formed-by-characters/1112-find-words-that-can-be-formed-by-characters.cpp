2a0b04bbe33f32ad6dfc734dbadd909f803e0f50
Dhia Eddine Thabet <93912178+diya-thabet@users.noreply.github.com>
2025-07-19 17:27:47 +0100

    Time: 24 ms (46%), Space: 21.5 MB (88.16%) - LeetHub

--- 1112-find-words-that-can-be-formed-by-characters/1112-find-words-that-can-be-formed-by-characters.cpp
+++ 1112-find-words-that-can-be-formed-by-characters/1112-find-words-that-can-be-formed-by-characters.cpp
@@ -1,20 +1,29 @@
 class Solution {
 public:
     int countCharacters(vector<string>& words, string chars) {
-        int res = 0; 
-        vector<int> c(26, 0); 
-        for(char cc : chars) c[cc-'a']++; 
-
-        for(auto s : words){
-            vector<int> w(26, 0); 
-            for(char cc : s) w[cc-'a']++;
-            for(int i = 0 ; i<26 ; i++){
-                if(w[i]>c[i]) goto label; 
-            }
-            res+= s.size(); 
-            label:
+        vector<int>v(26,0);
+        int l=0;
+        for(int i=0;i<chars.length();i++)
+        {
+            v[chars[i]-'a']++;
         }
-
-        return res; 
+        vector<int>v2=v;
+        int f=1;
+        for(int i=0;i<words.size();i++)
+        {
+            f=1;
+            string a=words[i];
+            for(int j=0;j<a.length();j++)
+            {
+                if(v[a[j]-'a']>0)
+                v[a[j]-'a']--;
+                else f=0;
+                
+            }
+            if(f==1)
+            l+=a.length();
+            v=v2;
+        }
+        return l;
     }
 };
