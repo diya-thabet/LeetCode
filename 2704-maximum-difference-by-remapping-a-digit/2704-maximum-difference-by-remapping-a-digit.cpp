105356c91ce68785b4e918a364e582db1869d67d
Dhia Eddine Thabet <93912178+diya-thabet@users.noreply.github.com>
2025-07-22 12:47:07 +0100

    Time: 0 ms (100%), Space: 7.9 MB (96.49%) - LeetHub

--- 2704-maximum-difference-by-remapping-a-digit/2704-maximum-difference-by-remapping-a-digit.cpp
+++ 2704-maximum-difference-by-remapping-a-digit/2704-maximum-difference-by-remapping-a-digit.cpp
@@ -7,19 +7,19 @@ public:
         for(int i = 0 ; i<s.size(); i++){
             if(s[i]=='9' && index == i) index++; else break; 
         }
-
-        int indexMin = 0; 
-        for(int i = 0 ; i<s.size(); i++){
-            if(s[i]=='0' && indexMin == i) index++; 
-            else break; 
-        }
-
         char c = s[index];
         for(int i = 0 ; i<s.size(); i++){
             if(s[i]==c ) s[i] = '9'; 
         }
 
-        c = mini[indexMin]; 
+        //mini
+        index = 0; 
+        for(int i = 0 ; i<s.size(); i++){
+            if(s[i]=='0' && index == i) index++; 
+            else break; 
+        }
+
+        c = mini[index]; 
         for(int i = 0 ; i<s.size(); i++){
             if(mini[i]==c ) mini[i] = '0'; 
         }
