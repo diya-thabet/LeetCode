68636bfe4a44ee7e329b8f992bb52ec1e2144e80
Dhia Eddine Thabet <93912178+diya-thabet@users.noreply.github.com>
2025-07-24 11:45:43 +0100

    Time: 3 ms (64.75%), Space: 17.9 MB (46.2%) - LeetHub

---
+++ 1407-group-the-people-given-the-group-size-they-belong-to/1407-group-the-people-given-the-group-size-they-belong-to.py
@@ -0,0 +1,14 @@
+class Solution:
+    def groupThePeople(self, groupSizes: List[int]) -> List[List[int]]:
+        l = {}
+        res = []
+        for i in range(len(groupSizes)):
+            if groupSizes[i] not in l:
+                l[groupSizes[i]] = []
+            l[groupSizes[i]].append(i)
+
+        for key in l:
+            group = l[key]
+            for i in range(0, len(group), key):
+                res.append(group[i:i + key])
+        return res
