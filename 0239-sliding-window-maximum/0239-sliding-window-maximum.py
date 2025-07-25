904519162d490c8464a2e641007517692d8aaaa5
Dhia Eddine Thabet <93912178+diya-thabet@users.noreply.github.com>
2025-07-25 09:52:11 +0100

    Time: 0 ms (100%), Space: 32 MB (31.09%) - LeetHub

--- 0239-sliding-window-maximum/0239-sliding-window-maximum.py
+++ 0239-sliding-window-maximum/0239-sliding-window-maximum.py
@@ -3,21 +3,21 @@ __import__("atexit").register(lambda: open("display_runtime.txt", "w").write("0"
 
 class Solution:
     def maxSlidingWindow(self, nums: List[int], k: int) -> List[int]:
-        dq = deque()  # Stores indices of useful elements
+        dq = deque()  # stores indices of useful elements
         result = []
 
         for i in range(len(nums)):
-            # Remove indices that are out of this window
+            # remove indices that are out of this window
             if dq and dq[0] < i - k + 1:
                 dq.popleft()
 
-            # Remove elements smaller than current from the back
+            # remove elements smaller than current from the back
             while dq and nums[dq[-1]] < nums[i]:
                 dq.pop()
 
             dq.append(i)
 
-            # Add max in current window
+            # add max in current window
             if i >= k - 1:
                 result.append(nums[dq[0]])
 
