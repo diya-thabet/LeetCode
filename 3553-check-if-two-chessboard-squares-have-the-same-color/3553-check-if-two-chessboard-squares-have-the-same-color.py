ac1a97de5ec934ad0278ded320b868d01ee39cce
Dhia Eddine Thabet <93912178+diya-thabet@users.noreply.github.com>
2025-07-26 08:57:43 +0100

    Time: 48 ms (6.27%), Space: 17.9 MB (21.76%) - LeetHub

---
+++ 3553-check-if-two-chessboard-squares-have-the-same-color/3553-check-if-two-chessboard-squares-have-the-same-color.py
@@ -0,0 +1,8 @@
+def squareIsWhite(coordinates: str) -> bool:
+        c = ord(coordinates[0]) - ord('a')
+        n = int(coordinates[1])
+        return (c+n)%2==0
+
+class Solution:
+    def checkTwoChessboards(self, coordinate1: str, coordinate2: str) -> bool:
+        return squareIsWhite(coordinate1) == squareIsWhite(coordinate2)
