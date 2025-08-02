bfd13049bd8ae8a4b1423e1b90d935165ab89f8d
Dhia Eddine Thabet <93912178+diya-thabet@users.noreply.github.com>
2025-08-02 11:37:00 +0100

    Time: 5 ms (56.51%), Space: 20.1 MB (12.28%) - LeetHub

--- 0653-two-sum-iv-input-is-a-bst/0653-two-sum-iv-input-is-a-bst.py
+++ 0653-two-sum-iv-input-is-a-bst/0653-two-sum-iv-input-is-a-bst.py
@@ -4,18 +4,22 @@
 #         self.val = val
 #         self.left = left
 #         self.right = right
-def dfs(root, l, v):
-    if root:
-        dfs(root.left, l, v)
-        dfs(root.right, l, v)
-        
-        l.append(root.val)
 class Solution:
     def findTarget(self, root: Optional[TreeNode], k: int) -> bool:
-        l=[]
-        dfs(root, l, k)
-        for i in range(len(l)-1):
-            for j in range(i+1, len(l)):
-                if l[i]+l[j] == k:
-                    return True
-        return False
+        
+        visited = set()
+
+        def dfs(root):
+
+            if not root:
+                return False
+
+            if k - root.val not in visited:
+                visited.add(root.val)
+                return dfs(root.left) or dfs(root.right)
+            else:
+                return True
+            
+            
+        
+        return dfs(root)
