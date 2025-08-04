401df9e7a79e318113fe4937440e352eb78204fc
Dhia Eddine Thabet <93912178+diya-thabet@users.noreply.github.com>
2025-08-04 20:59:17 +0100

    Time: 392 ms (35.76%), Space: 17.9 MB (32.83%) - LeetHub

--- 0441-arranging-coins/0441-arranging-coins.py
+++ 0441-arranging-coins/0441-arranging-coins.py
@@ -1,9 +1,8 @@
 class Solution:
     def arrangeCoins(self, n: int) -> int:
         i = 1
-        res= 0 
+       
         while n>0:
-            res+=1
             i+=1
             n-=i
-        return res
+        return i-1
