b614a1dacf0609b979f2e06663a0abe0d90508c8
Dhia Eddine Thabet <93912178+diya-thabet@users.noreply.github.com>
2025-08-05 12:54:48 +0100

    Time: 6 ms (53.87%), Space: 17.8 MB (96.65%) - LeetHub

--- 3093-sum-of-values-at-indices-with-k-set-bits/3093-sum-of-values-at-indices-with-k-set-bits.py
+++ 3093-sum-of-values-at-indices-with-k-set-bits/3093-sum-of-values-at-indices-with-k-set-bits.py
@@ -1,19 +1,3 @@
-def isK(n ,k):
-    res=0
-    l = str(n)
-    for i in range(33):
-        if n&1:
-            res+=1
-        n= n>>1
-        
-    return res==k
-
-
 class Solution:
     def sumIndicesWithKSetBits(self, nums: List[int], k: int) -> int:
-        print(isK(15, k))
-        res=0
-        for i in range(len(nums)):
-            if(isK(i, k)):
-                res+=nums[i]
-        return res
+        return sum(x for i, x in enumerate(nums) if bin(i).count('1') == k)
