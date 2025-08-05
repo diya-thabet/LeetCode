f25166270d645c416fb0e186339c79b3273aaa5f
Dhia Eddine Thabet <93912178+diya-thabet@users.noreply.github.com>
2025-08-05 16:34:06 +0100

    Time: 11 ms (76.88%), Space: 20.5 MB (78.54%) - LeetHub

--- 1254-deepest-leaves-sum/1254-deepest-leaves-sum.py
+++ 1254-deepest-leaves-sum/1254-deepest-leaves-sum.py
@@ -4,25 +4,18 @@
 #         self.val = val
 #         self.left = left
 #         self.right = right
-def dodo(root, s, niv, n) -> int:
-    if not(root):
-        return 0
-    if not(root.right) and not(root.left) and niv==n:
-        s+= root.val
-        return s
-    else:
-        return dodo(root.left, s,niv,n+1) + dodo(root.right, s, niv, n+1)
-
-def nivniv(root):
-    if not(root):
-        return 0
-    return max(1+ nivniv(root.left), 1+ nivniv(root.right))
-
 class Solution:
     def deepestLeavesSum(self, root: Optional[TreeNode]) -> int:
-        niv = nivniv(root)
-        print(niv)
-        res = dodo(root, 0, niv, 1)
-        return res
-
+        next_level = [root]
+        ans = 0
         
+        while next_level:
+            cur_level = next_level
+            next_level = []
+            for node in cur_level:
+                if node.left:
+                    next_level.append(node.left)
+                if node.right:
+                    next_level.append(node.right)
+        
+        return sum(node.val for node in cur_level)
