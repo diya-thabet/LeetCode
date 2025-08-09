9bd70979f69839d8849a6e5320c816fae05f99d0
Dhia Eddine Thabet <93912178+diya-thabet@users.noreply.github.com>
2025-08-09 13:02:53 +0100

    Time: 0 ms (100%), Space: 7.9 MB (17.71%) - LeetHub

--- 0231-power-of-two/0231-power-of-two.cpp
+++ 0231-power-of-two/0231-power-of-two.cpp
@@ -1,6 +1,16 @@
 class Solution {
 public:
     bool isPowerOfTwo(int n) {
-        return n > 0 && (n & (n - 1)) == 0;
+        int res = 1;
+        if(n<=0) return false; 
+        
+        for (int i = 0; i < 32; i++) {
+            if(n&1 == 1) res--; 
+            if(res <0) return false; 
+            n= n>>1; 
+        }
+        cout << res << endl; 
+
+        return true;
     }
 };
