fe678b815e9dd67b60c5b03d339aa83216efa3df
Dhia Eddine Thabet <93912178+diya-thabet@users.noreply.github.com>
2025-08-09 13:02:06 +0100

    Time: 0 ms (100%), Space: 8 MB (17.71%) - LeetHub

--- 0231-power-of-two/0231-power-of-two.cpp
+++ 0231-power-of-two/0231-power-of-two.cpp
@@ -1,9 +1,6 @@
 class Solution {
 public:
     bool isPowerOfTwo(int n) {
-        if(n==-16 || n==-2147483648) return false ; 
-        return __builtin_popcount(n) == 1 ; 
-       
-        
+        return n > 0 && (n & (n - 1)) == 0;
     }
 };
